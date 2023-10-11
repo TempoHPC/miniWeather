4d900a4a8c4a2546304009242e420f93186faffc
Isabel de Freitas <127349333+isabeldefreitas@users.noreply.github.com>
2023-10-11 19:32:57 -0300

    novas atualizações script das tabelas

---
+++ c/build/tabela_mpi_exemplo.sh
@@ -0,0 +1,49 @@
+cp processos.txt tabelaAnteriorMPI.txt;
+ 
+echo "MPI" > tabelaAtualMPI.txt
+ 
+ function CompFicticia(){
+	echo CompFicticia
+	echo "CPU Time:" $(((100+RANDOM%10)/$nucleos))
+
+}
+rm CPU_time_MPI.txt
+
+  for i in {1..5}; do
+    	np=(1 2 4 8 16 32);
+ 	for p in ${np[@]}; do 
+	nucleos=$p
+	#echo $nucleos "nucleo" >> processos.txt
+	CompFicticia | grep "CPU Time:" | cut -c10-  > CPU_time_MPI.txt
+
+	nucleos=$p
+	#echo $nucleos "nucleos" >> processos.txt
+	CompFicticia | grep "CPU Time:" | cut -c10- >> CPU_time_MPI.txt
+
+	nucleos=$p
+	#echo $nucleos "nucleos" >> processos.txt
+	CompFicticia | grep "CPU Time:" | cut -c10- >> CPU_time_MPI.txt
+
+	nucleos=$p
+	#echo $nucleos "nucleos" >> processos.txt
+	CompFicticia | grep "CPU Time:" | cut -c10- >> CPU_time_MPI.txt
+	
+	nucleos=$p
+	#echo $nucleos "nucleos" >> processos.txt
+	CompFicticia | grep "CPU Time:" | cut -c10- >> CPU_time_MPI.txt
+
+	nucleos=$p
+	#echo $nucleos "nucleos" >> processos.txt
+	CompFicticia | grep "CPU Time:" | cut -c10- >> CPU_time_MPI.txt
+
+
+	paste tabelaAnterior.txt CPU_time_MPI.txt >tabelaAtual.txt
+	cp tabelaAtual.txt tabelaAnterior.txt;
+    
+	#cat tabelaAtual.txt
+	done
+    done
+    
+    
+cat tabelaAtual.txt
+

17462342011dee1214fee76afe60eb32be7bcf66
isabeldefreitas <isabeldefreitasbarboza@gmail.com>
2023-12-25 23:23:11 -0300

    teste

--- c/build_scalasca/tabela_MPI.sh
+++ c/build_scalasca/tabela_MPI.sh
@@ -1,7 +1,6 @@
 rm tabelaMPI.txt
     
-        grep "CPU Time:" /scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca/scorep_sum_MPI-*_JOBID-*/slurm-*.out | sort -t- -k2,2 -n > tempos.txt
-        grep "CPU Time:" tempos.txt | cut -d ":" -f 3 >> CPU_time_MPI.txt
+        grep "CPU Time:" /scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca/scorep_sum_MPI-*_JOBID-*/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPU_time_MPI.txt
         paste nucleos.txt CPU_time_MPI.txt >> tabelaMPI.txt
 
 rm CPU_time_MPI.txt

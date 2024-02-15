036891376d314e4aa16b3a6f7527d314be40d5b9
rpsouto <rpsouto@lncc.br>
2024-02-15 15:13:15 -0300

    Remove unecessary files and folders

--- c/build/cmake_gnu_cpu_profiling.sh
+++ c/build/cmake_gnu_cpu_profiling.sh
@@ -6,8 +6,7 @@ export TEST_MPI_COMMAND="mpirun -n 1"
 
 PARALLEL_NETCDF_ROOT=$(spack location -i parallel-netcdf)
 
-cmake -DCMAKE_CXX_COMPILER=mpic++                                            \
-      -DCXXFLAGS="-Ofast -march=native -DNO_INFORM -std=c++11 -I${PARALLEL_NETCDF_ROOT}/include"   \
+cmake -DCXXFLAGS="-Ofast -march=native -DNO_INFORM -std=c++11 -I${PARALLEL_NETCDF_ROOT}/include"   \
       -DLDFLAGS="-L${PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"                        \
       -DOPENMP_FLAGS="-fopenmp"                                                       \
       -DNX=200                                                                        \

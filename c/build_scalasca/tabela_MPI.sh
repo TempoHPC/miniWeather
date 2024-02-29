e355cdc14d2971915121f093b0a7e705692d6364
isabeldefreitas <isabeldefreitasbarboza@gmail.com>
2024-02-29 17:31:34 -0300

    alteracao do script tabela_MPI.sh

--- c/build_scalasca/tabela_MPI.sh
+++ c/build_scalasca/tabela_MPI.sh
@@ -1,9 +1,11 @@
-rm tabelaMPI.txt
+rm tabela_MPI.txt
     
-        grep "CPU Time:" /scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca/scorep_sum_MPI-*_JOBID-*/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPU_time_MPI.txt
-        paste nucleos.txt CPU_time_MPI.txt >> tabelaMPI.txt
+        expDirB=/scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca
+        dirSpeedUp="scorep_sum_MPI-*_JOBID-*"
+        #grep "CPU Time:" /scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca/scorep_sum_MPI-*_JOBID-*/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPU_time_MPI.txt
+        grep "CPU Time:" $expDirB/$dirSpeedUp/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPUTime_MPI.txt
+        paste nucleos.txt CPUTime_MPI.txt >> tabela_MPI.txt
 
-rm CPU_time_MPI.txt
+rm CPUTime_MPI.txt
 
-
-cat tabelaMPI.txt
+cat tabela_MPI.txt

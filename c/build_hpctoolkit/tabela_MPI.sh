d3950da60f5b2fbea926827a1403d4153adbdfdc
isabeldefreitas <isabeldefreitasbarboza@gmail.com>
2024-02-29 18:27:39 -0300

    alteracao do script tabela_MPI.sh

--- c/build_hpctoolkit/tabela_MPI.sh
+++ c/build_hpctoolkit/tabela_MPI.sh
@@ -1,11 +1,16 @@
+
+#np=(1 2 4 8 16 24); printf '%s\n' "${np[@]}" >> nucleos.txt
+
+du profiling/ -d 3 | grep MPI|sort -n |awk -F- '{ print $3}'|awk -F_ '{ print $1}'  > nucleos.txt
+
 rm tabela_MPI.txt
     
         expDirB=/scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca
-        expDirB=/scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_hpctoolkit
+   export     expDirB=/scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_hpctoolkit
         dirSpeedUp="scorep_sum_MPI-*_JOBID-*"
-        dirSpeedUp="/profiling/hpctoolkit/NUMNODES-1/MPI-*_JOBID-*"
-        #grep "CPU Time:" /scratch/cenapadrjsd/isabel.barboza/miniWeather/c/build_scalasca/scorep_sum_MPI-*_JOBID-*/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPU_time_MPI.txt
-        grep "CPU Time:" $expDirB/$dirSpeedUp/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPUTime_MPI.txt
+   export     dirSpeedUp="profiling/hpctoolkit/NUMNODES-1/MPI-*_JOBID-*"
+        #grep "CPU Time:" $expDirB/$dirSpeedUp/slurm-*.out | sort -t- -k2,2 -n | cut -d ":" -f 3 > CPUTime_MPI.txt
+        du $expDirB/profiling/ -d 3 | grep MPI|sort -n |awk '{print "grep \"CPU Time:\"", $2"/slurm-*.out"}' | bash | cut -d ":" -f 2| cut  -d" " -f 2 >  CPUTime_MPI.txt
         paste nucleos.txt CPUTime_MPI.txt >> tabela_MPI.txt
 
 rm CPUTime_MPI.txt

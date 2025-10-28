30648fa21930f477347cdde70cb6969e18f80818
rpsouto <rpsouto@lncc.br>
2025-10-28 11:23:45 -0300

    new file:   env_sequana_nvhpc.sh

---
+++ fortran/build/env_sequana_nvhpc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module load git/2.23_sequana
+module load python/3.9.1_sequana
+module load gcc/9.3_sequana
+module load cmake/3.23.2_sequana
+module use /scratch/cenapadrjsd/rpsouto/opt/nvidia/hpc_sdk/modulefiles
+module load nvhpc/22.11
+export NVLOCALRC=/scratch/cenapadrjsd/rpsouto/opt/nvidia/hpc_sdk/Linux_x86_64/localrc
